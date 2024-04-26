14605f6f3da2d6f4c6f69b18928e95aa7cc71eb9
Vladislav1992 <kuryanov.vlad@yandex.ru>
2024-04-26 15:18:33 +0300

    minor edits before preparing the project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,26 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
+    "plugin:prettier/recommended", // v1
+    "prettier", //v1
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        singleQuote: true,
+        semi: true,
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="main">
+    <h1>This is MainPage</h1>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  background: white;
+  border-radius: 20px;
+}
+</style>
