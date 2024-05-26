b9809f8361f47cee3a1795c66f0d57830ee86062
Vladislav1992 <kuryanov.vlad@yandex.ru>
2024-05-26 20:22:11 +0300

    added firebase

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,22 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { initializeApp } from "firebase/app";
 
 import App from "./App.vue";
 import Antd from "ant-design-vue";
 import router from "./router";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeAWdKSf27dPMB_s3eTS3fuPz_ZcaCB34",
+  authDomain: "jwt-firebase-46e88.firebaseapp.com",
+  projectId: "jwt-firebase-46e88",
+  storageBucket: "jwt-firebase-46e88.appspot.com",
+  messagingSenderId: "732412129252",
+  appId: "1:732412129252:web:79da15b196fd9c1f4421f1",
+};
+
+initializeApp(firebaseConfig);
 
 const app = createApp(App);
 
